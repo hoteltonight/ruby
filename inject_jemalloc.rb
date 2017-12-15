83d46d3697148780b99e9f20179eb36ced8ea60d
Callum Jones <callum@hoteltonight.com>
2017-12-15 13:35:35 -0800

    Injection support

--- inject_jemalloc.rb
+++ inject_jemalloc.rb
@@ -5,7 +5,5 @@ dockerfiles.each do |file|
   text = File.read(file)
   text.gsub!(/apt-get install -y --no-install-recommends/, "apt-get install -y --no-install-recommends libjemalloc-dev")
   text.gsub!(/\.\/configure/, "./configure --with-jemalloc")
-
-  # To write changes to the file, use:
   File.open(file, "w") { |file| file.puts(text) }
 end
