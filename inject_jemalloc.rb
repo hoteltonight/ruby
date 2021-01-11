9e6e26e5b7e91e1a3fc9bd743276bed5b121c25e
Krzysztof Kołek <krzysztof.kolek@netguru.com>
2021-01-11 10:46:44 +0100

    Update script for jemalloc injection

--- inject_jemalloc.rb
+++ inject_jemalloc.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 path = File.expand_path(File.dirname(__FILE__))
-dockerfiles = Dir.glob("#{path}/**/Dockerfile")
+all_dockerfiles = Dir.glob("#{path}/**/Dockerfile")
+# Alpine Linux has dropped official support for jemalloc
+non_alpine_dockerfiles = all_dockerfiles.reject { |df| df.match("alpine") }
 
-dockerfiles.each do |file|
+non_alpine_dockerfiles.each do |file|
   text = File.read(file)
-  text.gsub!(/apt-get install -y --no-install-recommends/, "apt-get install -y --no-install-recommends libjemalloc-dev")
+  # Slim images are installing packages twice, we want to place jemalloc inside second one
+  # so we need to place file content inside one line, use keep operator (\K) with sub!
+  # to replace last occurrence and then gsub once again to place end line chars
+  text.gsub!("\n", "CRLF")
+      .sub!(/.*\Kapt-get install -y --no-install-recommends/, "apt-get install -y --no-install-recommends libjemalloc-dev")
+      .gsub!("CRLF", "\n")
   text.gsub!(/\.\/configure/, "./configure --with-jemalloc")
+  # Mark libjemalloc-dev as manually installed package to avoid removal via apt-get purge
+  text.gsub!(/apt-mark manual \$savedAptMark/, "apt-mark manual libjemalloc-dev $savedAptMark")
   File.open(file, "w") { |file| file.puts(text) }
 end
